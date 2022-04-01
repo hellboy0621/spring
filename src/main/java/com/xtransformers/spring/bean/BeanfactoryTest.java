e7c382e69f6e6f643426b213ac2dde9de6ada491
hellboy0621 <hero0621@126.com>
2022-04-01 22:58:25 +0800

    feat: Bean后处理器

--- src/main/java/com/xtransformers/spring/bean/BeanfactoryTest.java
+++ src/main/java/com/xtransformers/spring/bean/BeanfactoryTest.java
@@ -1,9 +1,11 @@
 package com.xtransformers.spring.bean;
 
+import java.util.Collection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.support.AbstractBeanDefinition;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
@@ -38,13 +40,40 @@ public class BeanfactoryTest {
         org.springframework.context.event.internalEventListenerFactory
          */
         // BeanFactory 后处理器，补充 Bean 定义
-        beanFactory.getBeansOfType(BeanFactoryPostProcessor.class).values().forEach(beanFactoryPostProcessor -> {
-            beanFactoryPostProcessor.postProcessBeanFactory(beanFactory);
-        });
+        beanFactory.getBeansOfType(BeanFactoryPostProcessor.class).values()
+                .forEach(beanFactoryPostProcessor -> beanFactoryPostProcessor.postProcessBeanFactory(beanFactory));
 
         for (String name : beanFactory.getBeanDefinitionNames()) {
             LOGGER.info(name);
         }
+
+        // Bean 后处理器，针对 Bean 的生命周期各个阶段提供扩展
+        // 处理 @Autowired
+        // org.springframework.context.annotation.internalAutowiredAnnotationProcessor
+        // 处理 @Resource
+        // org.springframework.context.annotation.internalCommonAnnotationProcessor
+
+        Collection<BeanPostProcessor> beanPostProcessors = beanFactory.getBeansOfType(BeanPostProcessor.class).values();
+        // 以下两种写法都是可以的
+        // beanPostProcessors.forEach(beanFactory::addBeanPostProcessor);
+        beanFactory.addBeanPostProcessors(beanPostProcessors);
+
+        // 默认情况下，懒汉式加载Bean 对象
+        // 可以显示使用 饿汉式加载 Bean 对象
+        beanFactory.preInstantiateSingletons();
+        LOGGER.info("breakline >>>>>>>>>>>>");
+
+        LOGGER.info("" + beanFactory.getBean(Bean1.class).getBean2());
+        /**
+         * BeanFactory 不会做
+         * 1. 不会主动调用 BeanFactory 后处理器
+         * 2. 不会主动添加 Bean 后处理器
+         * 3. 不会主动初始化单例
+         * 4. 不会解析 BeanFactory
+         * 5. 不会解析 ${} #{}
+         *
+         * BeanFactory 后处理器会有排序的逻辑
+         */
     }
 
     @Configuration
