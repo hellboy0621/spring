e4fae45c5252895375826bd65ca817f4c7cf85d1
hellboy0621 <hero0621@126.com>
2022-04-05 16:08:03 +0800

    feat: 常见Bean后处理器
    AutowiredAnnotationBeanPostProcessor
    CommonAnnotationBeanPostProcessor

--- src/main/java/com/xtransformers/spring/a04/A04Application.java
+++ src/main/java/com/xtransformers/spring/a04/A04Application.java
@@ -1,5 +1,8 @@
 package com.xtransformers.spring.a04;
 
+import org.springframework.beans.factory.annotation.AutowiredAnnotationBeanPostProcessor;
+import org.springframework.context.annotation.CommonAnnotationBeanPostProcessor;
+import org.springframework.context.annotation.ContextAnnotationAutowireCandidateResolver;
 import org.springframework.context.support.GenericApplicationContext;
 
 /**
@@ -16,6 +19,18 @@ public class A04Application {
         context.registerBean("bean1", Bean1.class);
         context.registerBean("bean2", Bean2.class);
         context.registerBean("bean3", Bean3.class);
+
+        // 解决 @Value 注入为 String 的变量
+        //         Caused by: org.springframework.beans.factory.NoSuchBeanDefinitionException: No qualifying bean of
+        //         type 'java.lang.String' available: expected at least 1 bean which qualifies as autowire candidate.
+        //         Dependency annotations: {@org.springframework.beans.factory.annotation.Value("${JAVA_HOME}")}
+        context.getDefaultListableBeanFactory()
+                .setAutowireCandidateResolver(new ContextAnnotationAutowireCandidateResolver());
+        // 解析 @Autowired @Value
+        context.registerBean(AutowiredAnnotationBeanPostProcessor.class);
+
+        // 解析 @Resource @PostConstruct @PreDestroy
+        context.registerBean(CommonAnnotationBeanPostProcessor.class);
 
         // 初始化容器
         context.refresh();
