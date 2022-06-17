2308855817185e77505c2da0cff2aa6fca1ed715
hellboy0621 <hero0621@126.com>
2022-06-17 22:38:22 +0800

    feat: Dispatcher v2.0 Dispatcher初始化时机及抽取配置

--- src/main/java/com/xtransformers/spring/a20/WebConfig.java
+++ src/main/java/com/xtransformers/spring/a20/WebConfig.java
@@ -1,22 +1,34 @@
 package com.xtransformers.spring.a20;
 
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletRegistrationBean;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.cglib.proxy.Dispatcher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
 // 不加参数，默认扫描配置类所在的包及其子包
 @ComponentScan
+@PropertySource("classpath:application.properties")
+/**
+ * 通过对象读取配置文件
+ * spring.web   -> WebProperties
+ * spring.mvc   -> WebMvcProperties
+ * server       -> ServerProperties
+ */
+// 会自动注入 WebMvcProperties 和 ServerProperties 类型的2个 Bean
+@EnableConfigurationProperties({WebMvcProperties.class, ServerProperties.class})
 public class WebConfig {
 
     // 内嵌 web 容器工厂
     @Bean
-    public TomcatServletWebServerFactory tomcatServletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory(ServerProperties serverProperties) {
+        return new TomcatServletWebServerFactory(serverProperties.getPort());
     }
 
     // 创建 DispatcherServlet
@@ -27,8 +39,13 @@ public class WebConfig {
 
     // 注册 DispatcherServlet，Spring MVC 入口
     @Bean
-    public DispatcherServletRegistrationBean dispatcherServletRegistrationBean(DispatcherServlet dispatcherServlet) {
-        return new DispatcherServletRegistrationBean(dispatcherServlet, "/");
+    public DispatcherServletRegistrationBean dispatcherServletRegistrationBean(
+            DispatcherServlet dispatcherServlet, WebMvcProperties webMvcProperties) {
+        DispatcherServletRegistrationBean registrationBean =
+                new DispatcherServletRegistrationBean(dispatcherServlet, "/");
+        // 在 tomcat 启动时初始化 DispatcherServlet
+        registrationBean.setLoadOnStartup(webMvcProperties.getServlet().getLoadOnStartup());
+        return registrationBean;
     }
 
 }
